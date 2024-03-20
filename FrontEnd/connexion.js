10a9ccdff6522ea4c76b52e52fe3a35851e9ced4
SilentCraftsman <ggambier72@gmail.com>
2024-03-20 17:01:18 +0100

    Création du système d'authentification sur connexion.js

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,1 +1,19 @@
-console.log("rzkbgqi");
+const baseApiUrl = "http://localhost:5678/api/";
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let form = {
+    email: document.getElementById("email"),
+    password: document.getElementById("password"),
+  };
+
+  fetch(`${baseApiUrl}users/login`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: form.email.value,
+      password: form.password.value,
+    }),
